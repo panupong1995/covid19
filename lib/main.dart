79e7982b5e7d3a4958dfbb9f0478ee1d10329cc5
martonjsupermart <panupong.sawas@gmail.com>
2023-12-09 16:23:06 +0700

    - add new screen info_screen, my_header

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:covid_19/constant.dart';
+import 'package:covid_19/widgets/counter.dart';
+import 'package:covid_19/widgets/my_header.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
@@ -35,59 +37,10 @@ class HomeScreen extends StatelessWidget {
     return Scaffold(
       body: Column(
         children: [
-          ClipPath(
-            clipper: MyClipper(),
-            child: Container(
-              padding: EdgeInsets.only(left: 40, top: 50, right: 20),
-              height: 350,
-              width: double.infinity,
-              decoration: const BoxDecoration(
-                gradient: LinearGradient(
-                  begin: Alignment.topRight,
-                  end: Alignment.bottomLeft,
-                  colors: [
-                    Color(0xFF3383CD),
-                    Color(0xFF11249F),
-                  ]
-                ),
-                image: DecorationImage(
-                  image: AssetImage("assets/images/virus.png")
-                ),
-              ),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Align(
-                    alignment: Alignment.topRight,
-                    child: SvgPicture.asset("assets/icons/menu.svg"),
-                  ),
-                  SizedBox(height: 20,),
-                  Expanded(
-                    child: Stack(
-                      children: [
-                        SvgPicture.asset(
-                          "assets/icons/Drcorona.svg",
-                          width: 230,
-                          fit: BoxFit.fitWidth,
-                          alignment: Alignment.topCenter,
-                        ),
-                        Positioned(
-                          top: 20,
-                          left: 150,
-                          child: Text(
-                            "All you need \nis stay at home.",
-                            style: kHeadingTextStyle.copyWith(
-                              color: Colors.white
-                            ),
-                          ),
-                        ),
-                        Container(), // I don't know why it can't work without container
-                      ],
-                    ),
-                  ),
-                ],
-              ),
-            ),
+          MyHeader(
+            image: "assets/icons/Drcorona.svg",
+            textTop: "All you need",
+            textBottom: "is stay at home.",
           ),
           Container(
             margin: EdgeInsets.symmetric(horizontal: 20),
@@ -170,41 +123,61 @@ class HomeScreen extends StatelessWidget {
                     ]
                   ),
                   child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
-                      Column(
-                        children: [
-                          Container(
-                            padding: EdgeInsets.all(6),
-                            height: 25,
-                            width: 25,
-                            decoration: BoxDecoration(
-                              shape: BoxShape.circle,
-                              color: kInfectedColor.withOpacity(.26),
-                            ),
-                            child: Container(
-                              decoration: BoxDecoration(
-                                shape: BoxShape.circle,
-                                color: Colors.transparent,
-                                border: Border.all(
-                                  color: kInfectedColor,
-                                  width: 2,
-                                ),
-                              ),
-                            ),
-                          ),
-                          SizedBox(height: 10),
-                          Text(
-                            "1046",
-                            style: TextStyle(
-                              fontSize: 40,
-                              color: kInfectedColor,
-                            ),
-                          ),
-                          Text("Infected", style: kSubTextStyle),
-                          Text("Infected", style: kSubTextStyle),
-                        ],
+                      Counter(
+                        color: kInfectedColor, 
+                        number: 1046,
+                        title: "Infected",
+                      ),
+                      Counter(
+                        color: kDeathColor, 
+                        number: 87,
+                        title: "Deaths",
+                      ),
+                      Counter(
+                        color: kRecoverColor, 
+                        number: 46,
+                        title: "Recovered",
                       ),
                     ],
+                  ),
+                ),
+                SizedBox(height: 20),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      "Spread of Virus", 
+                      style: kTitleTextStyle,
+                    ),
+                    Text(
+                      "See details", 
+                      style: TextStyle(
+                        color: kPrimaryColor, 
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                  ],
+                ),
+                Container(
+                  margin: EdgeInsets.only(top: 20),
+                  padding: EdgeInsets.all(20),
+                  height: 178,
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    color: Colors.white,
+                    boxShadow: [
+                      BoxShadow(offset: Offset(0, 10),
+                      blurRadius: 30,
+                      color: kShadowColor,
+                      ),
+                    ],
+                  ),
+                  child: Image.asset(
+                    "assets/images/map.png",
+                    fit: BoxFit.contain,
                   ),
                 ),
               ],
@@ -217,21 +190,6 @@ class HomeScreen extends StatelessWidget {
   
 }
 
-class MyClipper extends CustomClipper<Path> {  
-  @override
-  getClip(Size size) {
-    var path = Path();
-    path.lineTo(0, size.height - 80);
-    path.quadraticBezierTo(
-      size.width / 2, size.height, size.width, size.height - 80
-    );
-    path.lineTo(size.width, 0);
-    path.close();
-    return path;
-  }
 
-  @override
-  bool shouldReclip(covariant CustomClipper oldClipper) {
-    return false;
-  }
-}
+
+
